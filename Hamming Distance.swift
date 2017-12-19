d29a8984ca6c16304bfa83dd13c7a794e62b06e5
gaoxiang <gaoxiang@ezbuy.com>
2017-12-19 10:40:10 +0800

    461. Hamming Distance

---
+++ Hamming Distance.swift
@@ -0,0 +1,43 @@
+//
+//  Hamming Distance.swift
+//  LeetCode-Easy-Project
+//
+//  Created by admin on 2017/12/19.
+//  Copyright © 2017年 enjoy. All rights reserved.
+//
+
+import Foundation
+
+/**
+ The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ 
+ Given two integers x and y, calculate the Hamming distance.
+ 
+ Note:
+ 0 ≤ x, y < 2^31.
+ 
+ Input: x = 1, y = 4
+ 
+ Output: 2
+ 
+ Explanation:
+ 1   (0 0 0 1)
+ 4   (0 1 0 0)
+ ↑   ↑
+ 
+ The above arrows point to positions where the corresponding bits are different.
+ 
+ https://leetcode.com/problems/hamming-distance/
+
+ */
+
+class Solution {
+    func hammingDistance(_ x: Int, _ y: Int) -> Int {
+        guard x >= 0, y >= 0 else {
+            return 0
+        }
+        let result = String(x ^ y, radix: 2).filter { $0 == "1"}
+        return result.count
+    }
+}
+
