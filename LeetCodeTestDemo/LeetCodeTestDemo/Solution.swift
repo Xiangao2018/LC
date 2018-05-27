0d20070a2697f587e4ef9c7b97877ea904eb15ff
gaoxiang <gaoxiang@ezbuy.com>
2018-05-27 20:48:22 +0800

    add 771. Jewels and Stones

---
+++ LeetCodeTestDemo/LeetCodeTestDemo/Solution.swift
@@ -0,0 +1,11 @@
+//
+//  Solution.swift
+//  LeetCodeTestDemo
+//
+//  Created by enjoy on 2018/5/27.
+//  Copyright © 2018年 enjoy. All rights reserved.
+//
+
+import UIKit
+
+class Solution {}
---
+++ LeetCodeTestDemo/LeetCodeTestDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LeetCodeTestDemo
+//
+//  Created by enjoy on 2018/5/27.
+//  Copyright © 2018年 enjoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(Solution.numJewelsInStones("aA", "aAAbbbb"))
+    }
+
+
+}
+
